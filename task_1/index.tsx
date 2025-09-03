38eddaf48e371215b7f271a33babc262d1bfc673
Виктория Голубова <v.golubova@denexy.com>
2025-09-03 11:33:26 +0600

    Task 1 solved.

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, memo, PureComponent } from 'react';
 
 type IUser = {
     name: string
@@ -10,22 +10,27 @@ type IProps = {
 }
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = memo(({ name, age }: IUser) => (
     <div>
         my name is {name}, my age is {age}
     </div>
-);
+));
 
 // functional component
 // Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+const SecondComponent = memo(({ user: { name, age } }: IProps) => (
     <div>
         my name is {name}, my age is {age}
     </div>
-);
+), (prevProps: IProps, nextProps: IProps) => {
+    return (
+        nextProps.user.name === prevProps.user.name &&
+        nextProps.user.age === prevProps.user.age
+    );
+});
 
 // class component
-class ThirdComponent extends Component<IUser> {
+class ThirdComponent extends PureComponent<IUser> {
     render() {
         return (
             <div>
@@ -37,6 +42,12 @@ class ThirdComponent extends Component<IUser> {
 
 // class component
 class FourthComponent extends Component<IProps> {
+    shouldComponentUpdate(nextProps: IProps) {
+        return (
+            this.props.user.name !== nextProps.user.name ||
+            this.props.user.age !== nextProps.user.age
+        );
+    }
     render() {
         return (
             <div>
